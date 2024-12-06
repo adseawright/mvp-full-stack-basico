c6987e715cc7a1618ee80e1aa35ca18cfa0428fc
adseawright <aimone.rj@gmail.com>
2024-12-05 22:39:13 -0600

    Initial commit of MVP Full Stack project

---
+++ backend/database/models/material.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from database.models.base import Base
+from database.models.unidade import Unidade
+
+# Classe que representa a tabela 'materiais' no banco de dados
+class Material(Base):
+    __tablename__ = 'materiais'
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False)
+    quantidade = Column(Float, nullable=False)
+    custo_total = Column(Float, nullable=False)
+    unidade_id = Column(Integer, ForeignKey('unidades.id'), nullable=False)
+    #taxa = Column(Float, nullable=False, default=0.0) 
+
+    # Relacionamento com a classe Unidade para obter informações da unidade de medida do material
+    unidade = relationship("Unidade")
